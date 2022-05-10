72c8fe9db591f52d4256f8df066238e564e9d8c9
shivam803 <75017922+shivam803@users.noreply.github.com>
2022-05-10 10:00:39 +0530

    currently runninf if -else programs

---
+++ c++/length.cpp
@@ -0,0 +1,22 @@
+//Take values of length and breadth of a rectangle
+// from user and check if it is square or not.
+
+#include<iostream>
+using namespace std;
+#include <cmath>
+
+int main(){
+    int length;
+    int breadth;
+    cout<<"Enter the value of Length:"<<endl;
+    cin>>length;
+    cout<<"Enter the value of breadth:";
+    cin>>breadth;
+    
+    sqrt(length);
+    cout<<"length";
+    sqrt(breadth);
+    cout<<"breadth";
+
+    return 0;
+}
